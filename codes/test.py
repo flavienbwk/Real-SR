322c56b67ed71bebb4267f1356cdef7507aa2232
flavienbwk <flavien@berwick.fr>
2021-06-14 15:24:09 +0200

    Updated config and paths

--- codes/test.py
+++ codes/test.py
@@ -69,6 +69,7 @@ for test_loader in test_loaders:
             save_img_path = osp.join(dataset_dir, img_name + suffix + '.png')
         else:
             save_img_path = osp.join(dataset_dir, img_name + '.png')
+        print(f"Saving result image in {save_img_path}")
         util.save_img(sr_img, save_img_path)
 
         # calculate PSNR and SSIM
